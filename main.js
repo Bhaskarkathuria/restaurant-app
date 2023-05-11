8d5218cdf1a0dcdc3c1668b447b469a2cb255c27
Bhaskarkathuria <kathuriabhaskar712@gmail.com>
2023-05-11 22:47:24 +0530

    using Async await

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ table2.addEventListener('click',removeitem);
 table3.addEventListener('click',removeitem);
 
 
-function savetocloud(e){
+async function savetocloud(e){
 e.preventDefault();
 
 const price=priceInput.value;
@@ -36,8 +36,8 @@ if(price===""|| dish==="select"|| table==="select"){
     }
     else{
 
-axios.post("https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata",obj)
-.then((response)=>{
+const response=await axios.post("https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata",obj)
+
     console.log(response)
     let id;
 
@@ -66,84 +66,68 @@ axios.post("https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurant
     deletebtn.appendChild(document.createTextNode('delete'));
     li.appendChild(deletebtn)
 
-    })
+    }
 }
     //console.log(id)
 
 
 
     
-}
+
  
 
 // }
 
 
-function removeitem(e){
+async function removeitem(e){
+if(e.target.classList.contains('delete')) {
+if(confirm("Are you Sure!")) {
+const li=e.target.parentElement;
+const id=li.id;
+  li.remove();
+  try {
+    const response = await axios.delete(`https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata/${id}`);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+}
+}
 
-if(e.target.classList.contains('delete'))
-{
-    if(confirm("Are you Sure!"))
-    {
-        const li=e.target.parentElement
-        const id=li.id
 
-        li.remove();
-        //console.log(id)
+ window.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await axios.get("https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata");
+    let id;
+    for (let i = 0; i < response.data.length; i++) {
+      const price = response.data[i].price;
+      const dish = response.data[i].dish;
+      const table = response.data[i].table;
+      const li = document.createElement('li');
+      li.setAttribute('id', response.data[i]._id);
 
-        axios.delete(`https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata/${id}`)
-            .then((response) => {
-                console.log(response);
+      id = response.data[i]._id;
 
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        
-        
+      li.appendChild(document.createTextNode(`${price}Rs - ${dish} - ${table}`));
+
+      if (table === 'Table 1') {
+        table1.appendChild(li);
+      } else if (table === 'Table 2') {
+        table2.appendChild(li);
+      } else if (table === 'Table 3') {
+        table3.appendChild(li);
+      }
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.className = 'delete';
+      deleteBtn.appendChild(document.createTextNode('Delete'));
+      li.appendChild(deleteBtn);
+
+      console.log(li);
+      console.log(price, dish, table);
     }
-}
-
-}
-
- window.addEventListener('DOMContentLoaded', () => {
-      axios.get("https://crudcrud.com/api/eb90324b8dd94084b9291f3935552e99/restaurantdata")
-          .then((response) => {
-            // console.log(response);
-              let id;           
-                 for (let i = 0; i < response.data.length; i++) 
-              {   const price=response.data[i].price;
-                  const dish=response.data[i].dish;
-                  const table=response.data[i].table;
-                   const li = document.createElement('li');
-                   li.setAttribute('id',response.data[i]._id)
-                
-                   id=response.data[i]._id
-
-                   li.appendChild(document.createTextNode(`${price}Rs - ${dish} - ${table}`));
-
-                
-
-                   if (table === 'Table 1') {
-                       table1.appendChild(li);
-                   } else if (table === 'Table 2') {
-                       table2.appendChild(li);
-                   } else if (table === 'Table 3') {
-                       table3.appendChild(li);
-             }
-
-                   const deleteBtn = document.createElement('button');
-                   deleteBtn.className = 'delete';
-                   deleteBtn.appendChild(document.createTextNode('Delete'));                  
-                    li.appendChild(deleteBtn);
-
-                   console.log(li)
-                  console.log(price,dish,table)
-              }     
-          
-    
-          })
-          .catch((err) => {
-              console.log(err);
-          });
-  });
+  } catch (err) {
+    console.log(err);
+  }
+});
